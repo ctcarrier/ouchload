29ae4d5f6d0139bb3fc5602457fc85b52f20662e
Chris Carrier <ctcarrier@gmail.com>
2011-12-11 17:36:31 -0800

    Revert "Tons of changes getting async job management working"
    This reverts commit 2f7ec8362c78a7a62a0dd3582e1a78fd3cccf82c.

--- src/main/scala/com/ouchload/Run.scala
+++ src/main/scala/com/ouchload/Run.scala
@@ -1,8 +1,6 @@
 package com.ouchload
 
-import akka.actor.Actor
-import util.parsing.input.CharArrayReader
-import dsl._
+
 
 /**
  * @author chris_carrier
@@ -14,19 +12,12 @@ object Run {
 
   def main(args: Array[String]) {
 
-    //val e = new Executor()
-
-    val dsl = """
-    load "www.example.com" using 50 connections,
-    load "www.google.com" using 1 connections
-    """
-
-  LoadingDSL.parse(dsl)
-
-    //Thread.sleep(10000)
-
-    Actor.registry.shutdownAll
-    HttpRegistry.http.shutdown
-
+    new Executor()
+    sys.exit(1)
   }
+}
+
+class Executor extends Loader {
+
+  testWith(5) connections "www.efgdfgdfgdfgxample.com"
 }
--- src/main/scala/com/ouchload/exception/BadLoadDslException.scala
+++
@@ -1,8 +0,0 @@
-package com.ouchload.exception
-
-/**
- * @author chris_carrier
- * @version 12/11/11
- */
-
-class BadLoadDslException(message: String) extends Exception(message)
